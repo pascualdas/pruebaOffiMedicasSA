ca6bece0006c4ccbb178321745b6b796091b22e3
pascualdas <pascualdas@gmail.com>
2020-08-13 23:49:14 -0500

    CargueBDDatosPruebas

--- bd/pruebaoffimedicassa.sql
+++ bd/pruebaoffimedicassa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 14, 2020 at 03:33 AM
+-- Generation Time: Aug 14, 2020 at 06:48 AM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.4
 
@@ -41,13 +41,9 @@ CREATE TABLE `familiares` (
 --
 
 INSERT INTO `familiares` (`id`, `id_usuario`, `parentesco`, `creado_en`) VALUES
-(1, 1, 'Familiar', '2020-08-12 00:00:00'),
-(4, 1, 'Conyugue', '2020-08-13 00:00:00'),
-(5, 1, 'Hermano', '2020-08-13 00:00:00'),
-(7, 1, 'Familiar', '2020-08-13 16:08:50'),
-(11, 1, 'Otro', '2020-08-13 16:08:59'),
-(12, 2, 'Hermano', '2020-08-13 17:08:10'),
-(13, 2, 'Conyugue', '2020-08-13 17:08:44');
+(1, 1, 'Otro', '2020-08-13 21:08:32'),
+(2, 1, 'Hijo', '2020-08-13 21:08:07'),
+(3, 3, 'Hijo', '2020-08-13 22:08:39');
 
 -- --------------------------------------------------------
 
@@ -74,17 +70,13 @@ CREATE TABLE `personas` (
 --
 
 INSERT INTO `personas` (`id`, `dni`, `nombres`, `apellidos`, `direccion`, `telefono`, `correo`, `creado_en`, `tipoEntidad`, `id_tipoPersona`) VALUES
-(1, 1102714782, 'Dany Pascual', 'Gomez Sanchez', 'Real De Minas', '3012788000', 'pascualdas@gmail.com', '2020-08-12 00:00:00', 'Usuario', 1),
-(3, 1098668224, 'Carlos Alberto', 'Barros', 'Centro de bucaramanga', '3112558005', 'betoweb@gmail.com', '2020-08-13 00:00:00', 'Usuario', 2),
-(5, 1098668224, 'Carlos Alberto', 'Barros', 'Centro de bucaramanga', '3112558005', 'betoweb@gmail.com', '2020-08-13 00:00:00', 'Familiar', 1),
-(8, 1234, 'Lizet', 'Benavides', 'real', '32121321', 'livabe@gmail.com', '2020-08-13 00:00:00', 'Familiar', 4),
-(9, 1235, 'Pedro Alonso', 'Gomez Sanchez', 'adasdasd', '341321321', 'petter@gmail.com', '2020-08-13 00:00:00', 'Familiar', 5),
-(11, 1234656, 'Daniela', 'Benavides', 'Bogota', '3254654654', 'dany@gmail.com', '2020-08-13 16:08:50', 'Familiar', 7),
-(15, 123123123, 'Camilo', 'Nino', 'Canaveral', '123123123', 'cami@gmail.com', '2020-08-13 16:08:59', 'Familiar', 11),
-(16, 1102714782, 'Dany Pascual', 'Gomez Sanchez', 'RM', '3012788000', 'pascualdas@gmail.com', '2020-08-13 17:08:10', 'Familiar', 12),
-(17, 2147483647, 'Grace', 'Arvilla', 'centro', '65454541561', 'ga@gmail.com', '2020-08-13 17:08:44', 'Familiar', 13),
-(23, 1234, 'Lizet', 'Benavides', 'real', '32121321', 'livabe@gmail.com', '2020-08-13 18:08:30', 'Usuario', 6),
-(24, 159753, 'Libardo', 'Gomez', 'Real de minas', '321212121', 'ligoro@gmail.com', '2020-08-13 19:08:35', 'Usuario', 7);
+(1, 1102714782, 'Dany Pascual', 'Gomez Sanchez', 'Real De Minas', '3012788000', 'pascualdas@gmail.com', '2020-08-13 21:08:52', 'Usuario', 1),
+(2, 123, 'Jhon', 'Doe', 'Bucarmanga', '573012788001', 'jhondoe@gmail.com', '2020-08-13 21:08:32', 'Familiar', 1),
+(3, 321, 'Mark', 'zuckerberg', 'EEUU', '573157854561', 'mz@facebook.com', '2020-08-13 21:08:07', 'Familiar', 2),
+(4, 321, 'Mark', 'zuckerberg', 'EEUU', '573157854561', 'mz@facebook.com', '2020-08-13 21:08:34', 'Usuario', 2),
+(5, 13579, 'Steve', 'Wozniak', 'Luna', '57313245552', 'sw@gnu.lx', '2020-08-13 22:08:16', 'Usuario', 3),
+(6, 24682, 'Laura Gimena', 'Peralta Rubio', 'Carrera 32W No. 71-139', '0376973918 ', 'laura.peralta@offimedicas.com', '2020-08-13 22:08:39', 'Familiar', 3),
+(7, 24682, 'Laura Gimena', 'Peralta Rubio', 'Carrera 32W No. 71-139', '0376973918 ', 'laura.peralta@offimedicas.com', '2020-08-13 22:08:15', 'Usuario', 4);
 
 -- --------------------------------------------------------
 
@@ -106,10 +98,10 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `contrasena`, `is_active`, `creado_en`, `ultimo_acceso`) VALUES
-(1, '123321', '1', '2020-08-12 00:00:00', '2020-08-13 19:08:39'),
-(2, '123321', '1', '2020-08-13 00:00:00', '2020-08-13 18:08:40'),
-(6, '123', '1', '2020-08-13 18:08:30', '2020-08-13 19:08:17'),
-(7, 'Designer1', '1', '2020-08-13 19:08:35', '2020-08-13 19:08:43');
+(1, 'Qwerty0713', '1', '2020-08-13 21:08:52', '2020-08-13 23:08:24'),
+(2, 'Facebook123', '1', '2020-08-13 21:08:34', '2020-08-13 22:08:06'),
+(3, 'Linux123', '1', '2020-08-13 22:08:16', '2020-08-13 22:08:32'),
+(4, 'Gestion12345', '1', '2020-08-13 22:08:15', '2020-08-13 22:08:18');
 
 --
 -- Indexes for dumped tables
@@ -142,19 +134,19 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT for table `familiares`
 --
 ALTER TABLE `familiares`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `personas`
 --
 ALTER TABLE `personas`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
